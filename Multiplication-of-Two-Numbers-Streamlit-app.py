4dd20c081c08777916c70b8d6249226d465d08d2
Akshay-ASRK <67980923+Akshay-ASRK@users.noreply.github.com>
2022-07-21 06:07:56 +0530

    Add files via upload

---
+++ Multiplication-of-Two-Numbers-Streamlit-app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+st.write("""
+# Multiplication of Two Numbers App
+
+This app will provide you the multiplication of two given Numbers.
+""")
+
+st.header("User Inputs")
+
+def user_inputs():
+  A=st.number_input("Enter A Value :")
+  B=st.number_input("Enter B Value :")
+
+  C=float(A)*float(B)
+  d={'A':A,'B':B,'C':C}
+  return d
+
+D=user_inputs()
+
+if st.button("Multiply"):
+  a="{:.2f}".format(D['A'])
+  b="{:.2f}".format(D['B'])
+  c="{:.2f}".format(D['C'])
+  st.subheader("Result : "+str(c))

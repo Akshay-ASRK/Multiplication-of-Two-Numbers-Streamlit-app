c00b2a1cbde00fb4221709ef1c768e8dc2ad0f89
Akshay-ASRK <67980923+Akshay-ASRK@users.noreply.github.com>
2022-07-22 12:49:21 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,14 @@
 mkdir -p ~/.streamlit/
+
+
+echo "\
+[general]\n\
+email = \"akshaykarpe515@yahoo.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS = false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml

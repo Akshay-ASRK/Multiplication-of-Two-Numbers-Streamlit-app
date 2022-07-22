de48e6259f1560eb3d86332601d940370f8eadc0
Akshay-ASRK <67980923+Akshay-ASRK@users.noreply.github.com>
2022-07-22 14:19:40 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,12 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"akshaykarpe515@yahoo.com"\\n\
+" > ~/.streamlit/credentials.toml
+
+
+server.listen(config.port, function () {
+  console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
+});
 

d614f5218c7a87ab3c7a7efcb0cb8d06ddf896e0
Akshay-ASRK <67980923+Akshay-ASRK@users.noreply.github.com>
2022-07-22 14:07:19 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,17 @@
 mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"akshaykarpe515@yahoo.com"\\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS = false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
+
+server.listen(config.port, function () {
+  console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
+});
